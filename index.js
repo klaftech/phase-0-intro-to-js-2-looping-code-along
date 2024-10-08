2542f04b1f5ab902563994ac33b962202da94515
Elchonon <ekn400@gmail.com>
2024-10-08 15:57:45 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const mesgArray = [];
+
+function writeCards(pplArray,event){
+  //for(i=0; i < pplArray.length; i++){
+  let i=0;
+  while(i < pplArray.length){
+    mesgArray.push(`Thank you, ${pplArray[i]}, for the wonderful ${event} gift!`);
+    i++;
+  }
+  return mesgArray;
+}
+
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+//console.log(mesgArray);
+
+function countDown(number){
+    //console.log(number);
+    for(number; number >= 0; number--){
+        console.log(number);
+        //debugger;
+        //return number;
+    }
+}
+
+//countDown(4);
